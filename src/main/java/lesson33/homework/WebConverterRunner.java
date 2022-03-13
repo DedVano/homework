8a24d57ef813d8482d9f259f108b415e038ecdac
DedVano <ivmcorp@yandex.ru>
2022-03-13 22:48:31 +0300

    added homework for lesson 33

---
+++ src/main/java/lesson33/homework/WebConverterRunner.java
@@ -0,0 +1,12 @@
+package lesson33.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebConverterRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebConverterRunner.class, args);
+    }
+}
