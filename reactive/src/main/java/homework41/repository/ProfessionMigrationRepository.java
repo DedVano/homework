4f656f474abc06e4d60fb59b2f322ab3ba72c309
DedVano <ivmcorp@yandex.ru>
2022-04-20 15:01:10 +0300

    added homework for lesson 41

--- reactive/src/main/java/homework41/repository/ProfessionMigrationRepository.java
+++ reactive/src/main/java/homework41/repository/ProfessionMigrationRepository.java
@@ -3,6 +3,5 @@ package homework41.repository;
 import homework41.model.Profession;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-
 public interface ProfessionMigrationRepository extends MongoRepository<Profession, Integer> {
 }
--- reactive/src/main/java/homework41/repository/ProfessionRepository.java
+++ reactive/src/main/java/homework41/repository/ProfessionRepository.java
@@ -3,6 +3,5 @@ package homework41.repository;
 import homework41.model.Profession;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-
 public interface ProfessionRepository extends ReactiveMongoRepository<Profession, Integer> {
 }
